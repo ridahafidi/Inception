e01c955fc80a240ac12bd883145b3bba2bf75569
ridahafidi <ridahafidi5@gmail.com>
2026-07-06 21:15:09 +0100

    mysql cant log as root without pass

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -6,6 +6,7 @@ mkdir -p /run/mysqld
 chown -R mysql:mysql /run/mysqld #/var/lib/mysql
 
 DB_PASSWORD=$(cat /run/secrets/db_password)
+ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
 mariadbd --user=mysql --skip-networking &
 
@@ -14,7 +15,8 @@ do
 	sleep 1
 done
 
-mariadb -u root << EOF
+if mariadb -u root -e "SELECT 1" >/dev/null 2>&1; then
+	mariadb -u root << EOF
 CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
 
 CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%'
@@ -22,9 +24,25 @@ IDENTIFIED BY '${DB_PASSWORD}';
 
 GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
 
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
+
 FLUSH PRIVILEGES;
 EOF
+else
+	mariadb -u root -p"${ROOT_PASSWORD}" << EOF
+CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
 
-mariadb-admin -u root shutdown
+CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%'
+IDENTIFIED BY '${DB_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
+
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
+
+FLUSH PRIVILEGES;
+EOF
+fi
+
+mariadb-admin -u root -p"${ROOT_PASSWORD}" shutdown
 
 exec mariadbd --user=mysql
