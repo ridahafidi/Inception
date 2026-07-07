f2691633a7509dc64ba830855758d1d95fa9b6c4
ridahafidi <ridahafidi5@gmail.com>
2026-07-07 18:16:29 +0100

    small comments changes

--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -10,7 +10,7 @@ WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 
-#copy wp files if needed
+#copy wp files if wp-confing.php dosen't exist
 if [ ! -f wp-config.php ]; then
     cp -r /tmp/wordpress/* /var/www/html/ &&
     rm -rf /tmp/wordpress 
@@ -18,9 +18,9 @@ fi
 
 cd /var/www/html
 
-#create wp-confing.php
-#wp config create generates wp-config.php and stores the
-#database connection information.
+#if wp-confing.php dosen't exist
+#create wp-config.php that has the database infos
+#to reach out to db when needed
 if [ ! -f wp-config.php ]; then
 wp config create \
     --dbname="${MARIADB_DATABASE}" \
@@ -30,7 +30,7 @@ wp config create \
     --allow-root
 fi
 
-#wait for mariadb
+#check if mariadb is ready and test a real TCP connection to database and a query test
 until mariadb \
     -h"${WORDPRESS_DB_HOST}" \
     -u"${MARIADB_USER}" \
@@ -40,9 +40,9 @@ do
     sleep 2
 done
 
-#wp core install uses those credentials to connect to
-#MariaDB, create all WordPress tables, initialize the site,
-#and create the administrator account.
+#check if wp tables re configured in database using wp-cli
+#if not wp core install creates all the necessary db tables and insert admin
+#instead of reaching to install.php wp-cli handles it
 if ! wp core is-installed --allow-root; then
 wp core install \
     --url="${DOMAIN_NAME}" \
@@ -53,7 +53,8 @@ wp core install \
     --allow-root
 fi
 
-#create wp_user if it does not already exist
+#check if user42 exists in database
+# if not create it
 if ! wp user get "${WP_USER}" --allow-root >/dev/null 2>&1; then
 wp user create \
     "${WP_USER}" \
@@ -64,6 +65,7 @@ wp user create \
 fi
 
 #set www-data as owner and group of /var/ww/html
+#to avoid access conflict with nginx during upload requests
 chown -R www-data:www-data /var/www/html
 
 echo "Starting PHP-FPM..."
