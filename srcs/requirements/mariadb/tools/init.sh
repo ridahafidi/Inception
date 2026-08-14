cfbf000ba0fc53588a1d770c55fdd87f65b99a0a
ridahafidi <ridahafidi5@gmail.com>
2026-08-14 19:20:21 +0100

    done

--- srcs/requirements/mariadb/tools/init.sh
+++ srcs/requirements/mariadb/tools/init.sh
@@ -8,6 +8,9 @@ chown -R mysql:mysql /run/mysqld
 DB_PASSWORD=$(cat /run/secrets/db_password)
 ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
+if [ ! -d /var/lib/mysql/wp_db ];
+then
+
 mariadbd --user=mysql --skip-networking &
 
 until mariadb-admin ping >/dev/null 2>&1
@@ -15,34 +18,21 @@ do
 	sleep 1
 done
 
-if mariadb -u root -e "SELECT 1" >/dev/null 2>&1; then
 	mariadb -u root << EOF
-CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
+	CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
 
-CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%'
-IDENTIFIED BY '${DB_PASSWORD}';
+	CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%'
+	IDENTIFIED BY '${DB_PASSWORD}';
 
-GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
+	GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
 
-FLUSH PRIVILEGES;
+	FLUSH PRIVILEGES;
 EOF
-else
-	mariadb -u root -p"${ROOT_PASSWORD}" << EOF
-CREATE DATABASE IF NOT EXISTS ${MARIADB_DATABASE};
-
-CREATE USER IF NOT EXISTS '${MARIADB_USER}'@'%'
-IDENTIFIED BY '${DB_PASSWORD}';
-
-GRANT ALL PRIVILEGES ON ${MARIADB_DATABASE}.* TO '${MARIADB_USER}'@'%';
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
-
-FLUSH PRIVILEGES;
-EOF
-fi
 
 mariadb-admin -u root -p"${ROOT_PASSWORD}" shutdown
+
+fi
 
 exec mariadbd --user=mysql
